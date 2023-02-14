571a1197f4daff4d4b57be418c8b02dc7f2bacea
Administrator <admin@example.com>
2023-02-14 16:53:32 +0200

    Fixed options 'true/false' issue, fixed network netmask if in network options user gives ip instead of whole network

--- analyzer.py
+++ analyzer.py
@@ -15,7 +15,7 @@ def Analyzer(nemo):
 
         # lock.acquire()
         pricli.Printlnr("Searching for hosts...")
-        network_scanner.NetworkDiscovery(network_status.network)
+        network_scanner.NetworkDiscovery(nemo.network)
         network_status.Update()
 
         # pricli.Clear()
